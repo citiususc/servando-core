013bc06a890245b858e50659f9153d34575216b0
Jose Ángel Piñeiro Souto <joseangel.pineiro@usc.es>
2013-02-27 12:54:58 +0100

    Added support for action finishing in ServicefragmentView. Subclasses can now call completeExecutionAndFinish() to do that.

--- src/es/usc/citius/servando/android/ui/ServiceFragmentView.java
+++ src/es/usc/citius/servando/android/ui/ServiceFragmentView.java
@@ -3,11 +3,15 @@ package es.usc.citius.servando.android.ui;
 import java.util.Collections;
 import java.util.List;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.widget.Toast;
 import es.usc.citius.servando.android.models.protocol.MedicalActionExecution;
 
 public abstract class ServiceFragmentView extends Fragment {
+
+	private static final String APPLICATION_PACKAGE = "es.usc.citius.servando.android.app";
+	private static final String HOME_ACTIVITY = "es.usc.citius.servando.android.app.activities.PatientHomeActivity";
 
 	public interface ServiceFragmentCloseListener {
 		public void onCloseFragment();
@@ -73,6 +77,38 @@ public abstract class ServiceFragmentView extends Fragment {
 	public void updateUi()
 	{
 		// default implementation does nothing
+	}
+
+	protected void completeExecution()
+	{
+		if (execution != null)
+		{
+			new Thread(new Runnable()
+			{
+
+				@Override
+				public void run()
+				{
+					execution.finish(getActivity().getApplication());
+				}
+			}).start();
+		}
+	}
+
+	protected void completeExecutionAndFinish()
+	{
+		completeExecution();
+		Intent i = new Intent();
+		i.setClassName(APPLICATION_PACKAGE, HOME_ACTIVITY);
+		i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+		if (execution != null)
+		{
+			i.putExtra("removing", execution.getUniqueId());
+		}
+
+		getActivity().startActivity(i);
+		onCloseView();
 	}
 
 }
